74a3c8a8f05d517c2d2dba937d270a92348128f9
l-Luna <izzeldeen03@gmail.com>
2023-01-06 19:33:48 +0000

    start work on metadata configuration screen

---
+++ Loenn/libraries/altSidesMeta.lua
@@ -0,0 +1,90 @@
+﻿local metaHelper = {}
+
+-- list and group available options
+
+metaHelper.options = {}
+
+-- Map/OverrideVanillaSideData are handled specially
+metaHelper.options.meta = {
+    { "Preset", "option", { "none", "a-side", "b-side", "c-side", "d-side" } },
+    { "UnlockMode", "option", { "consecutively", "always", "triggered", "with_previous", "c_sides_unlocked" } },
+    { "OverrideHeartTextures", "boolean" },
+    title = "meta"
+}
+
+metaHelper.options.overworld = {
+    { "Icon", "string", { "menu/play", "menu/remix", "menu/rmx2", "menu/leppa/AltSidesHelper/rmx3" } },
+    { "Label", "string", { "OVERWORLD_NORMAL", "OVERWORLD_REMIX", "OVERWORLD_REMIX2", "leppa_AltSidesHelper_overworld_remix3" } },
+    { "DeathsIcon", "string", { "collectables/skullBlue", "collectables/skullRed", "collectables/skullGold" } },
+    { "ChapterPanelHeartIcon", "string", { "collectables/heartgem/0/spin", "collectables/heartgem/1/spin", "collectables/heartgem/2/spin", "collectables/leppa/AltSidesHelper/heartgem/dside" } },
+    { "JournalHeartIcon", "string", { "heartgem0", "heartgem1", "heartgem2", "leppa/AltSidesHelper/heartgemD" } },
+    { "ShowBerriesAsGolden", "boolean" },
+    title = "overworld"
+}
+
+metaHelper.options.inGame = {
+    { "InWorldHeartIcon", "string", { "collectables/heartGem/0", "collectables/heartGem/1", "collectables/heartGem/2", "collectables/heartGem/3" } },
+    { "EndScreenTitle", "string", { "AREACOMPLETE_NORMAL", "AREACOMPLETE_BSIDE", "AREACOMPLETE_CSIDE", "leppa_AltSidesHelper_areacomplete_dside" } },
+    { "HeartColour", "color", { "8cc7fa", "ff668a", "fffc24", "ffffff" } },
+    { "ShowHeartPoem", "boolean" },
+    { "ShowBSideRemixIntro", "boolean" },
+    title = "inGame"
+}
+
+metaHelper.options.fullClear = {
+    { "EndScreenClearTitle", "string", { "AREACOMPLETE_NORMAL_FULLCLEAR", "leppa_AltSidesHelper_areacomplete_fullclear_bside", "leppa_AltSidesHelper_areacomplete_fullclear_cside", "leppa_AltSidesHelper_areacomplete_fullclear_dside" } },
+    { "CanFullClear", "boolean" },
+    { "CassetteNeededForFullClear", "boolean" },
+    { "HeartNeededForFullClear", "boolean" },
+    title = "fullClear"
+}
+
+metaHelper.options.experimental = {
+    { "JournalCassetteIcon", "string", { "cassette", "leppa/AltSidesHelper/cassetteD" } },
+    { "AddCassetteIcon", "boolean" },
+    title = "experimental"
+}
+
+metaHelper.orderedOptions = {
+    metaHelper.options.meta,
+    metaHelper.options.overworld,
+    metaHelper.options.inGame,
+    metaHelper.options.fullClear,
+    metaHelper.options.experimental
+}
+
+-- load options
+
+--- load an `altsideshelper.meta.yaml` file from a full path.
+---@param path string
+---@return table
+function metaHelper.loadMetaByPath(path)
+    local yaml = require("lib.yaml.reader")
+    local filesystem = require("utils.filesystem")
+
+    if filesystem.isFile(path) then
+        local f = assert(io.open(file, "rb"))
+        local content = f:read("*all")
+        f:close()
+        
+        return yaml.eval(content)
+    else
+        return {}
+    end
+end
+
+--- load the current map's `altsideshelper.meta.yaml` file, if it exists.
+--- returns a table containing loaded options (empty if it doesn't exist), or nil if the current map is not saved.
+---@return table
+function metaHelper.loadMeta()
+    local loadedState = require("loaded_state")
+    local baseFile = loadedState.filename
+    if not baseFile then
+        return nil
+    end
+    
+    local metaFile = string.sub(baseFile, 1, -5)
+    return metaHelper.loadMetaByPath(metaFile)
+end
+
+return metaHelper
---
+++ Loenn/ui/windows/altSidesMetaButton.lua
@@ -0,0 +1,107 @@
+﻿local mods = require("mods")
+
+local utils = require("utils")
+
+local uiElements = require("ui.elements")
+local widgetUtils = require("ui.widgets.utils")
+local form = require("ui.forms.form")
+local languageRegistry = require("language_registry")
+local notifications = require("ui.notification")
+
+local altSidesMeta = mods.requireFromPlugin("libraries.altSidesMeta")
+
+--
+
+local function intoGroups(tables)
+    local groups = {}
+
+    for _, tb in ipairs(tables) do
+        local group = {}
+        group["title"] = "ui.leppa.altsideshelpermeta.group." .. tb.title
+        local fields = {}
+
+        -- ipairs ignores the non-numeric `title` field
+        for _, field in ipairs(tb) do
+            table.insert(fields, field[1])
+        end
+
+        group["fieldOrder"] = fields
+        table.insert(groups, group)
+    end
+    
+    return groups
+end
+
+local function intoInfos(tables)
+    local infos = {}
+
+    for _, tb in ipairs(tables) do
+        for _, field in ipairs(tb) do
+            local info = {}
+
+            if field[2] == "option" then
+                info["fieldType"] = "string"
+                info["editable"] = false
+            else
+                info["fieldType"] = field[2]
+            end
+
+            if #field > 2 then
+                info["options"] = field[3]
+            end
+
+            infos[field[1]] = info
+        end
+    end
+
+    return infos
+end
+
+--
+
+local metaButton = uiElements.group({})
+
+metaButton.open = function (element)
+    local language = languageRegistry.getLanguage()
+    
+    local windowTitle = tostring(language.ui.leppa.altsideshelpermeta.title)
+
+    groups = utils.deepcopy(intoGroups(altSidesMeta.orderedOptions))
+    for _, group in ipairs(groups) do
+        if group.title then
+            local parts = group.title:split(".")()
+            local baseLanguage = utils.getPath(language, parts)
+            group.title = tostring(baseLanguage.name)
+        end
+    end
+    
+    local metadataForm = form.getForm({}, {}, {
+        fields = intoInfos(altSidesMeta.orderedOptions),
+        groups = groups
+    })
+
+    local window = uiElements.window(windowTitle, metadataForm):with({
+        x = windowX,
+        y = windowY,
+
+        updateHidden = true
+    })
+
+    metaButton.parent:addChild(window)
+    widgetUtils.addWindowCloseButton(window)
+    form.prepareScrollableWindow(window)
+
+    return window
+end
+
+-- thanks Just Loenny Things
+
+local menubar = require("ui.menubar")
+local mapButton = $(menubar.menubar):find(t -> t[1] == "map")
+
+if not $(mapButton[2]):find(e -> e[1] == "leppa_altsides_meta") then
+    table.insert(mapButton[2], {})
+    table.insert(mapButton[2], { "leppa_altsides_meta", metaButton.open })
+end
+
+return metaButton
